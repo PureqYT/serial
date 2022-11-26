532c2896d1e4c55dbf99407ec3c18ba6d8050a13
unknown <pureq999@gmail.com>
2022-11-26 16:22:59 +0100

    fin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 const { SerialPort } = require('serialport');
-const Readline = require('@serialport/parser-readline');
-const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
-const parser = port.pipe(new Readline({ delimiter: '\n' }));
-// Read the port data
-port.on("open", () => {
-  console.log('serial port open');
-});
-parser.on('data', data =>{
-  console.log('got word from arduino:', data);
+const { ReadlineParser } = require('@serialport/parser-readline');
+const fs = require("fs");
+require('dotenv').config();
+
+const port = new SerialPort({
+  path: process.env.PORT,
+  baudRate: parseInt(process.env.BAUDRATE),
+})  
+
+const parser = port.pipe(new ReadlineParser({ delimiter: '\u200b' }))
+parser.on('data', (data) => {
+    let currentdate = new Date(); 
+    let datetime = "Time: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+    // console.log(datetime);
+    // console.log(data);
+    fs.appendFile("./data/results.txt", datetime + data + '\n', (err) => {
+        if (err) console.error(err);
+        return;
+    });
 });
